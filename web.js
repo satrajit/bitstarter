2d91e3cc99fafe4052bab0d1082430bd96166eb1
satrajit <satrajit.gupta@gmail.com>
2013-07-23 22:47:42 +0000

    modified to point to a file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var stringTest = (fs.readFileSync('index.html')).toString();
+  response.send(stringTest);
+
 });
 
 var port = process.env.PORT || 5000;
